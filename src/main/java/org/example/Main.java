17e03139183be2336b3c09d6f28d51d0698858b4
Cesar Estiven Medrano <cesar-2007-mesa@hotmail.com>
2025-10-22 01:00:21 -0500

    feat: Defiicion matriz y datos de llenado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,31 @@
 package org.example;
 
+import java.util.Scanner;
+
+class Matriz{
+    int n; //Tamaño de matriz cuadrada
+    double m [][];     //Datos
+
+    void capturarDatos(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese tamaño de la matriz: ");
+        n = sc.nextInt();
+        m = new double[n][n];
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print( " Ingrese el elemento de matriz (" + i + " , " + j + "):");
+                m[i][j] = sc.nextDouble();
+            }
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Matriz m1 = new Matriz();
+
+        m1.capturarDatos();
+
     }
 }
